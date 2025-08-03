5135a2a676501fdb3bfe3efe65ead81154ba53c1
victoraranha19 <aldo.victor1610@gmail.com>
2025-08-03 13:00:21 -0300

    first commit

---
+++ src/main/java/com/victor/DisorderSpringApplication.java
@@ -0,0 +1,13 @@
+package com.victor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisorderSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DisorderSpringApplication.class, args);
+	}
+
+}
